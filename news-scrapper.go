a2a2e99cb99eb7a0d74cf9457ee3d0372a2721a8
Santiago Rodriguez <santo1996.29@gmail.com>
2025-06-03 16:08:08 -0400

    moved visit statement to happen after the handler fn

--- news-scrapper.go
+++ news-scrapper.go
@@ -7,7 +7,6 @@ import "github.com/gocolly/colly"
 type NewsScrapper struct {
 	collector *colly.Collector
 	url       string
-	Data      []NewsData
 }
 
 // NewNewsScrapper creates am already initialized NewsScrapper.
@@ -15,15 +14,18 @@ func NewNewsScrapper(url string) *NewsScrapper {
 	return &NewsScrapper{
 		collector: colly.NewCollector(),
 		url:       url,
-		Data:      make([]NewsData, 0),
 	}
 }
 
-// Scrape leaves the scrapping exact process to the client,
-// while providing a channel that the user must make use
-// of to know if the scrapping is done.
-func (s *NewsScrapper) Scrape(handler func(c *colly.Collector, errch chan<- error)) chan<- error {
+// Scrape leaves the scraping exact process to the client criteria,
+// and does the scraping in a separate goroutine while providing a
+// channel that the user must make use of to know if the scraping is done.
+func (s *NewsScrapper) Scrape(handler func(c *colly.Collector, datach chan<- NewsData, errch chan<- error)) (<-chan NewsData, <-chan error) {
 	errch := make(chan error)
-	handler(s.collector, errch)
-	return errch
+	datach := make(chan NewsData)
+	go func() {
+		handler(s.collector, datach, errch)
+		s.collector.Visit(s.url)
+	}()
+	return datach, errch
 }
